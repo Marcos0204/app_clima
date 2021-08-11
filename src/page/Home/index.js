21f399fb6dafc59da4355c31fbd0981f8dcb5565
Marcos <marcosbastidas301@gmail.com>
2021-08-11 11:22:43 -0400

    add_clima

--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -4,14 +4,19 @@ import Clima from '../../components/clima'
 import { Container, makeStyles } from '@material-ui/core';
 
 
-const useStyles= makeStyles((theme)=>{
-   
-} )
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display:'flex',
+      height: '100vh',
+      alignItems: 'center',
+      justifContent: 'space-between !importan',
+    },
+  }));
 
 const Home = () => {
     const classes= useStyles()
     return (
-        <Container maxWidth="lg">
+        <Container maxWidth="lg" className={classes.root}>
             <Form />
             <Clima />
         </Container>
